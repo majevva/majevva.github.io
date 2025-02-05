b686ec60f93f14206139fa4cd7ea36e5413e089b
majevva <majevvskitomek@gmail.com>
2025-02-05 12:51:44 +0100

    hacker theme

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    new fullpage('#fullpage', {
-        autoScrolling: true,
-        navigation: true,
-        anchors: ['start', 'about', 'contact'],
-        navigationTooltips: ['Start', 'O mnie', 'Kontakt'],
-        showActiveTooltip: true,
-    });
-});
